22b2620f0151161aab0a2abe52f89bc3301af83a
Yanfei Zhou <zyanfei@qti.qualcomm.com>
2021-07-23 16:58:06 +0800

    AVRCP_CT: Expose API get_element_attribute_cmd
    This feature is required by PTS test cases AVRCP/CT/CA.
    
    Change-Id: I1ec92acf0b04bee9f7f9034fa16263b621eea910

--- system/btif/src/btif_rc.cc
+++ system/btif/src/btif_rc.cc
@@ -1617,7 +1617,7 @@ static void handle_notification_response(tBTA_AV_META_MSG* pmeta_msg, tAVRC_REG_
              */
              rc_start_play_status_timer(p_dev);
           }
-          get_element_attribute_cmd(AVRC_MAX_NUM_MEDIA_ATTR_ID, attr_list, p_dev);
+          get_metadata_attribute_cmd(p_dev->rc_addr, attr_list_size, attr_list);
         } else {
           rc_stop_play_status_timer(p_dev);
         }
@@ -3480,7 +3480,7 @@ static bt_status_t get_metadata_attribute_cmd(const RawAddress& bd_addr,
 static bt_status_t get_element_attribute_cmd(const RawAddress& bd_addr,
                                              uint8_t num_attribute,
                                              uint32_t* p_attr_ids) {
-  log::verbose("num_attribute: {} attribute_id {}", num_attribute, p_attr_ids[0]);
+  log::verbose("num_attribute: {}", num_attribute);
   btif_rc_device_cb_t* p_dev = btif_rc_get_device_by_bda(bd_addr);
   if (p_dev == NULL) {
     log::error("p_dev NULL");
@@ -3488,6 +3488,24 @@ static bt_status_t get_element_attribute_cmd(const RawAddress& bd_addr,
   }
 
   CHECK_RC_CONNECTED(p_dev);
+
+  // For IOP Issue
+  // Few peer devices without CA featue forcibly restart BT
+  // when it receives request to retrieve value of attribute
+  // AVRC_MEDIA_ATTR_ID_COVER_ART.
+  uint32_t attr_list[] = {
+    AVRC_MEDIA_ATTR_ID_TITLE,       AVRC_MEDIA_ATTR_ID_ARTIST,
+    AVRC_MEDIA_ATTR_ID_ALBUM,       AVRC_MEDIA_ATTR_ID_TRACK_NUM,
+    AVRC_MEDIA_ATTR_ID_NUM_TRACKS,  AVRC_MEDIA_ATTR_ID_GENRE,
+    AVRC_MEDIA_ATTR_ID_PLAYING_TIME
+  };
+
+  if (!(p_dev->rc_features & BTA_AV_FEAT_COVER_ARTWORK) &&
+    num_attribute == AVRC_MAX_NUM_MEDIA_ATTR_ID) {
+    p_attr_ids = attr_list;
+    num_attribute = sizeof(attr_list)/sizeof(attr_list[0]);
+  }
+
   tAVRC_COMMAND avrc_cmd = {0};
   avrc_cmd.get_elem_attrs.opcode = AVRC_OP_VENDOR;
   avrc_cmd.get_elem_attrs.status = AVRC_STS_NO_ERROR;
@@ -3722,6 +3740,7 @@ static const btrc_ctrl_interface_t bt_rc_ctrl_interface = {
         request_continuing_response_cmd,
         abort_continuing_response_cmd,
         get_item_attribute_cmd,
+        get_element_attribute_cmd,
         cleanup_ctrl,
 };
 
