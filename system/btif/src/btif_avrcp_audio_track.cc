0209969c47e7afff0a0eda911e23f5617894b78f
Ying Wu <yingwu@qti.qualcomm.com>
2024-07-25 17:00:23 +0800

    A2DP_SINK: Use AAUDIO_PERFORMANCE_MODE_HD_APTX flag for aptX-HD
    Change-Id: I98fe06110426a524b16b9125a5a493c7b7963e1f

--- system/btif/src/btif_avrcp_audio_track.cc
+++ system/btif/src/btif_avrcp_audio_track.cc
@@ -109,7 +109,7 @@ void* BtifAvrcpAudioTrackCreate(int trackFreq, int bitsPerSample, int channelCou
   AAudioStreamBuilder_setChannelCount(builder, channelCount);
   AAudioStreamBuilder_setSessionId(builder, AAUDIO_SESSION_ID_ALLOCATE);
   aaudio_performance_mode_t mode = (bitsPerSample >= 24) ?
-      AAUDIO_PERFORMANCE_MODE_NONE : AAUDIO_PERFORMANCE_MODE_LOW_LATENCY;
+      AAUDIO_PERFORMANCE_MODE_HD_APTX : AAUDIO_PERFORMANCE_MODE_LOW_LATENCY;
   log::debug("mode:{}", mode);
   AAudioStreamBuilder_setPerformanceMode(builder, mode);
   AAudioStreamBuilder_setErrorCallback(builder, ErrorCallback, nullptr);
