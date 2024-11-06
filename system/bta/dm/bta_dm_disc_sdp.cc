ece63ba8f6027526699056d299d808c55031795c
Yanfei Zhou <zyanfei@qti.qualcomm.com>
2024-11-06 12:14:25 +0530

    SDP: Do not search for PCE if PSE service is not enabled
    Issue description:
    PBAP downloading is not triggered due to not receiving
    PBAP PCE SDP response from peer device.
    
    Root cause:
    By default, DUT initiates PBAP PCE service search if
    remote device is not in denylisted for PCE SDP.
    
    In Android Auto, DUT plays the role as PBAP PCE while remote
    device plays the role as PBAP PSE. This may take long time
    for remote device responding or remote device doesn't respond
    to PCE service search request thoroughly. This results in
    block in initiating phonebook downloading.
    
    Solution:
    Do not search for PCE if PSE service is not enabled.
    
    Change-Id: Ifd119b66b7c83937813ecfbe631ebe64f70d1c43
    Signed-off-by: Yanfei Zhou <quic_zyanfei@quicinc.com>

--- system/bta/dm/bta_dm_disc_sdp.cc
+++ system/bta/dm/bta_dm_disc_sdp.cc
@@ -27,6 +27,7 @@
 
 #include "bta/dm/bta_dm_disc_int.h"
 #include "bta/include/bta_sdp_api.h"
+#include "btif/include/btif_common.h"
 #include "btif/include/btif_config.h"
 #include "btif/include/btif_storage.h"
 #include "device/include/interop.h"
@@ -346,8 +347,11 @@ void bta_dm_sdp_find_services(tBTA_DM_SDP_STATE* sdp_state) {
 
   if (uuid == Uuid::From16Bit(UUID_PROTOCOL_L2CAP)) {
     if (!is_sdp_pbap_pce_disabled(sdp_state->bd_addr)) {
-      log::debug("SDP search for PBAP Client");
-      BTA_SdpSearch(sdp_state->bd_addr, Uuid::From16Bit(UUID_SERVCLASS_PBAP_PCE));
+      tBTA_SERVICE_MASK service_mask = btif_get_enabled_services_mask();
+      if(service_mask & (tBTA_SERVICE_MASK)(1 << BTA_PBAP_SERVICE_ID)) {
+        log::debug("SDP search for PBAP Client");
+        BTA_SdpSearch(sdp_state->bd_addr, Uuid::From16Bit(UUID_SERVCLASS_PBAP_PCE));
+      }
     }
   }
   sdp_state->service_index++;
