0a09a58fae20ad60673e5a6bad2210f0448f2cfa
Fei Zheng <zfei@qti.qualcomm.com>
2024-11-12 17:44:01 +0800

    A2DP_SINK: Discard old data before audio focus is granted
    Issue found:
    Sometimes A2DP streaming is inaudible even though A2DP is in playing
    status.
    
    Root Cause:
    In current implementation of btif_a2dp_sink_enqueue_buf(), size of
    rx_audio_queue reaches its maximum capacity when audio focus has not
    been granted yet, which impacts start of decoding queued A2DP packets.
    Consequently, the queue is always in full status and decoding can't be
    executed.
    
    Resolution:
    Discard old data before audio focus is granted to avoid the queue being
    full.
    
    CRs-Fixed: 4022442
    
    Change-Id: I496314512c8f0e110903cfd2600ff0780bcfa2b1
    (cherry picked from commit 3d3c002b87c1138bd20abaae9130aeb55122cabe)
    (cherry picked from commit 2005a6e1b935d67a03d9334b77b54fe27ab05366)

--- system/btif/src/btif_a2dp_sink.cc
+++ system/btif/src/btif_a2dp_sink.cc
@@ -667,6 +667,10 @@ uint8_t btif_a2dp_sink_enqueue_buf(BT_HDR* p_pkt) {
     if (btif_a2dp_sink_cb.rx_focus_state == BTIF_A2DP_SINK_FOCUS_GRANTED) {
       log::info("Request to begin decoding");
       btif_a2dp_sink_audio_handle_start_decoding();
+    } else {
+      // Discard old packet to prevent the queue from being full which
+      // results in the decoding process to be bypassed
+      osi_free(fixed_queue_try_dequeue(btif_a2dp_sink_cb.rx_audio_queue));
     }
   }
 
