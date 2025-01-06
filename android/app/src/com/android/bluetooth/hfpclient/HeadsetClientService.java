e48841cf63e477e94c0f1524e3f590c4e853bf08
Kiran Pachava <quic_kpachava@quicinc.com>
2025-01-06 15:28:59 +0530

    Revert "HFP_HF: Use CarAudioManager to adjust stream volume"
    This reverts commit b8af7e1eb0c4c3a4b888e145d2b81e42c191fd6b.
    
    Change-Id: I0c67e6bbcb9a43438548ad9e148dddee8cdf513e

--- android/app/src/com/android/bluetooth/hfpclient/HeadsetClientService.java
+++ android/app/src/com/android/bluetooth/hfpclient/HeadsetClientService.java
@@ -14,9 +14,8 @@
  * limitations under the License.
  *
  * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
- *
- * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause-Clear.
+ * Copyright (c) 2024-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 package com.android.bluetooth.hfpclient;
@@ -34,7 +33,6 @@ import android.bluetooth.IBluetoothHeadsetClient;
 import android.content.AttributionSource;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.AudioManager;
@@ -134,10 +132,9 @@ public class HeadsetClientService extends ProfileService {
                 mStateMachineMap.clear();
             }
 
-            IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-            if (!isAutomotive()) {
-                filter.addAction(AudioManager.ACTION_VOLUME_CHANGED);
-            }
+            IntentFilter filter = new IntentFilter(AudioManager.ACTION_VOLUME_CHANGED);
+            filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+            filter.addAction(Intent.ACTION_BATTERY_CHANGED);
             registerReceiver(mBroadcastReceiver, filter);
 
             // Start the HfpClientConnectionService to create connection with telecom when HFP
@@ -1340,9 +1337,5 @@ public class HeadsetClientService extends ProfileService {
                 }
             }
         }
-    }
-
-    boolean isAutomotive() {
-        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_AUTOMOTIVE);
     }
 }
