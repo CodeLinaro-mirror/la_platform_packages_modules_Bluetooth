111bf92afcb33e5ab96657172cb60ea246d24341
Song Gao <quic_songg@quicinc.com>
2025-02-17 16:23:57 +0800

    Partially revert "Initialize main thread earlier"
    Issue found:
    Observe fatal exception sometimes when disabling Bluetooth.
    
    Root cause:
    Defect is introduced AOSP upgrading with the code commit
    14c424fdfda12787b7e96e32e34e5033de6ca20f. SDP connection timer and BTM
    control block resources are accessed by thread bt_main_thread after
    these resources have already been freed and cleaned up.
    
    Resolution:
    Revert the change of main_thread_shut_down() and reserve the change of
    main_thread_start_up() to ensure that thread bt_main_thread can be
    shutdown before cleaning up BTM and SDP connection timer.
    
    Change-Id: Ifa8e0d7140c1fd17a24acf79de1ce1400128c630
    (cherry picked from commit 7b8befa48ad749a70f9738113cba81289b3b9d03)

--- system/btif/src/stack_manager.cc
+++ system/btif/src/stack_manager.cc
@@ -14,6 +14,11 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
+ *  Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ *
+ *  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *  SPDX-License-Identifier: BSD-3-Clause-Clear
+ *
  ******************************************************************************/
 
 #define LOG_TAG "bt_stack_manager"
@@ -372,6 +377,8 @@ static void event_shut_down_stack(ProfileStopCallback stopProfiles) {
 
   future_await(local_hack_future);
 
+  main_thread_shut_down();
+
   gatt_free();
   sdp_free();
   l2c_free();
@@ -434,8 +441,6 @@ static void event_clean_up_stack(std::promise<void> promise, ProfileStopCallback
   module_shut_down(get_local_module(GD_SHIM_MODULE));
 
   module_clean_up(get_local_module(OSI_MODULE));
-
-  main_thread_shut_down();
 
   module_management_stop();
   info("finished");
