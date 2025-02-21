831bf42e5308c50721a3839b4f2ffe2c50e5ee57
Song Gao <songg@qti.qualcomm.com>
2025-02-21 16:33:34 +0800

    HCI_HAL: Do HCI close before HCI initialization
    Issue found:
    HCI HAL asserts due receiving non-success status returned by callback
    initializationComplete during user switch stress testing.
    
    Root cause:
    In the process of user switch, onUnbind() in BluetoothAdapterService
    is not invoked by system promptly. Therefore, BluetoothHci::close()
    is not invoked to do BT HAL cleanup during user switch. When the new
    Bluetooth process is started for new user, invalid status of
    initializationComplete is returned because BT HAL service is ever in
    initialized status. This results in BT stack aborting itself.
    
    Resolution:
    Invoke close() before each invocation of initialization().
    
    Change-Id: Ie82447f1337d4a1192ba2035f5492a0dce969785

--- system/gd/hal/hci_backend_aidl.cc
+++ system/gd/hal/hci_backend_aidl.cc
@@ -12,6 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ *
+ * Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #include <aidl/android/hardware/bluetooth/BnBluetoothHci.h>
@@ -94,6 +99,7 @@ public:
   }
 
   void initialize(std::shared_ptr<HciBackendCallbacks> callbacks) {
+    hci_->close();
     hci_callbacks_ = ::ndk::SharedRefBase::make<AidlHciCallbacks>(callbacks);
     hci_->initialize(hci_callbacks_);
   }
