36390e0225c9430f7c664d10b50fdd2465e0bd46
Song Gao <songg@qti.qualcomm.com>
2025-04-22 09:54:56 +0800

    HFP_HF: Support to release specified call
    PTS case HFP/HF/ECC/BV-01-C for "AT+CHLD=1x"
    
    Change-Id: I4c2d1e2cdb0048c8b9335c79ef8932428de73c15

--- android/app/aidl/android/bluetooth/IBluetoothHeadsetClient.aidl
+++ android/app/aidl/android/bluetooth/IBluetoothHeadsetClient.aidl
@@ -12,6 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ *
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear.
  */
 
 package android.bluetooth;
@@ -62,6 +67,8 @@ interface IBluetoothHeadsetClient {
     boolean rejectCall(in BluetoothDevice device, in AttributionSource attributionSource);
     @JavaPassthrough(annotation="@android.annotation.RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)")
     boolean terminateCall(in BluetoothDevice device, in BluetoothHeadsetClientCall call, in AttributionSource attributionSource);
+    @JavaPassthrough(annotation="@android.annotation.RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)")
+    boolean releaseCall(in BluetoothDevice device, in int index, in AttributionSource attributionSource);
 
     @JavaPassthrough(annotation="@android.annotation.RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)")
     boolean enterPrivateMode(in BluetoothDevice device, int index, in AttributionSource attributionSource);
--- android/app/src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
+++ android/app/src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
  *
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
- * SPDX-License-Identifier: BSD-3-Clause-Clear
+ * SPDX-License-Identifier: BSD-3-Clause-Clear.
  */
 
 // Bluetooth Headset Client State Machine
@@ -125,6 +125,7 @@ public class HeadsetClientStateMachine extends StateMachine {
     public static final int SEND_VENDOR_AT_COMMAND = 21;
     public static final int SEND_BIEV = 22;
     public static final int SEND_ANDROID_AT_COMMAND = 23;
+    public static final int RELEASE_CALL = 24;
 
     // internal actions
     @VisibleForTesting static final int QUERY_CURRENT_CALLS = 50;
@@ -767,6 +768,24 @@ public class HeadsetClientStateMachine extends StateMachine {
             addQueuedAction(ENTER_PRIVATE_MODE, c);
         } else {
             error("ERROR: Couldn't enter private id:" + idx);
+        }
+    }
+
+    @VisibleForTesting
+    private void releaseCall(int idx) {
+        debug("releaseCall: " + idx);
+
+        HfpClientCall c = mCalls.get(idx);
+
+        if (c == null || c.getState() != HfpClientCall.CALL_STATE_ACTIVE) {
+            return;
+        }
+
+        if (mNativeInterface.handleCallAction(
+                mCurrentDevice, HeadsetClientHalConstants.CALL_ACTION_CHLD_1X, idx)) {
+            addQueuedAction(RELEASE_CALL, c);
+        } else {
+            error("ERROR: Couldn't release call " + " id:" + idx);
         }
     }
 
@@ -1492,6 +1511,7 @@ public class HeadsetClientStateMachine extends StateMachine {
                 case HOLD_CALL -> holdCall();
                 case TERMINATE_CALL -> terminateCall();
                 case ENTER_PRIVATE_MODE -> enterPrivateMode(message.arg1);
+                case RELEASE_CALL -> releaseCall(message.arg1);
                 case EXPLICIT_CALL_TRANSFER -> explicitCallTransfer();
                 case SEND_DTMF -> {
                     if (mNativeInterface.sendDtmf(mCurrentDevice, (byte) message.arg1)) {
