1337c9af60531f561747a8f53be26c428172d147
Wentong Jin <quic_wentjin@quicinc.com>
2025-05-22 14:00:10 +0530

    Search PACS services when reconnection remotes
    Root cuase: Some remotes need host to search PACS before
    sends any ASE op cmds when reconnection.
    
    Change-Id: Ib43f8383898a95b9f164e69ecffe8add681292e7
    (cherry picked from commit 7b4ead2dcf5f3cce8a309ab17ca8658c4934a0ba)
    (cherry picked from commit 0e3d917fe0a31f9df8e4f42bf150a62de9a47492)
    (cherry picked from commit f100f67e98cc455f9d7202f49bf975c83d081cc5)
    (cherry picked from commit aa469921742039685b8bff374ed47e39083c420d)

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -13,6 +13,12 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ *
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear.
+ *
  */
 
 #include <base/functional/bind.h>
@@ -2013,6 +2019,10 @@ public:
       }
     }
 
+    //For BT reboot cases, remotes need PACS discover.
+    leAudioDevice->known_service_handles_ = false;
+    btif_storage_leaudio_clear_service_data(address);
+
     leAudioDevice->autoconnect_flag_ = autoconnect;
     /* When adding from storage, make sure that autoconnect is used
      * by all the devices in the group.
@@ -2874,6 +2884,7 @@ public:
 
   void OnGattDisconnected(tCONN_ID conn_id, tGATT_IF /*client_if*/, RawAddress address,
                           tGATT_DISCONN_REASON reason) {
+    log::info("OnGattDisconnected");
     LeAudioDevice* leAudioDevice = leAudioDevices_.FindByConnId(conn_id);
 
     if (!leAudioDevice) {
@@ -2893,6 +2904,10 @@ public:
     leAudioDevice->closing_stream_for_disconnection_ = false;
     leAudioDevice->encrypted_ = false;
     leAudioDevice->acl_phy_update_done_ = false;
+
+    log::info("Remove service data, addr: {}", address);
+    leAudioDevice->known_service_handles_ = false;
+    btif_storage_leaudio_clear_service_data(address);
 
     auto connection_state = leAudioDevice->GetConnectionState();
 
@@ -6668,7 +6683,9 @@ void le_audio_gattc_callback(tBTA_GATTC_EVT event, tBTA_GATTC* p_data) {
       break;
 
     case BTA_GATTC_SRVC_DISC_DONE_EVT:
-      instance->OnGattServiceDiscoveryDone(p_data->service_discovery_done.remote_bda);
+      /*PACS read would be done when encryption complete*/
+      log::warn("Needn't do PACS when BTA_GATTC_SRVC_DISC_DONE_EVT.");
+      //instance->OnGattServiceDiscoveryDone(p_data->service_discovery_done.remote_bda);
       break;
 
     case BTA_GATTC_SRVC_CHG_EVT:
