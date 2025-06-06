2c6c2619fb8b2ce2306563da77572b23f0a53f7c
dongcao <dongcao@qti.qualcomm.com>
2025-06-06 11:15:16 +0530

    BLE Audio: Conditionally use Read Multiple for initial ASE read
    Issue:
    Can not connect BAP profile with BLE Audio device
    
    Root Cause:
    AOSP use Read Multiple for initial ASE read by default in commit
    b500a2a840476e4f6e51bfc2c081ea27fb4ab11a. But some BLE device doesn't support Read Multiple Variable Length, and DUT can't initiate remote
    ASE information by Read Multiple Variable command.
    
    Solution:
    If GATT_READ_MULT_VARIABLE_LENGTH equals true, DUT will use Read Multiple for initial ASE read. GATT_READ_MULT_VARIABLE_LENGTH is configured as false by default.
    
    CRs-Fixed: 4180632
    
    Change-Id: I988faf045625d3bd30fbe40f16302e983e3f0034
    (cherry picked from commit 154ed60e2594a9ccf8778a8947fac534826c7498)

--- system/bta/vc/device.cc
+++ system/bta/vc/device.cc
@@ -13,6 +13,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ *
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #include <bluetooth/log.h>
@@ -422,7 +427,7 @@ bool VolumeControlDevice::EnqueueInitialRequests(tGATT_IF gatt_if, GATT_READ_OP_
  */
 void VolumeControlDevice::EnqueueRemainingRequests(tGATT_IF /*gatt_if*/,
                                                    GATT_READ_OP_CB chrc_read_cb,
-                                                   GATT_READ_MULTI_OP_CB chrc_multi_read_cb,
+                                                   [[maybe_unused]] GATT_READ_MULTI_OP_CB chrc_multi_read_cb,
                                                    GATT_WRITE_OP_CB /*cccd_write_cb*/) {
   const auto is_eatt_supported = gatt_profile_get_eatt_support_by_conn_id(connection_id);
 
@@ -456,6 +461,7 @@ void VolumeControlDevice::EnqueueRemainingRequests(tGATT_IF /*gatt_if*/,
   log::debug("{}, number of variable-size attribute handles={}", address,
              handles_to_read_variable_length.size());
 
+#if (GATT_READ_MULT_VARIABLE_LENGTH == TRUE)
   if (com_android_bluetooth_flags_le_ase_read_multiple_variable() && is_eatt_supported) {
     const size_t payload_limit = this->mtu_ - 1;
 
@@ -489,7 +495,11 @@ void VolumeControlDevice::EnqueueRemainingRequests(tGATT_IF /*gatt_if*/,
       BtaGattQueue::ReadCharacteristic(connection_id, handle, chrc_read_cb, nullptr);
     }
   }
-
+#else
+  for (auto const& [handle, _] : handles_to_read) {
+    BtaGattQueue::ReadCharacteristic(connection_id, handle, chrc_read_cb, nullptr);
+  }
+#endif
   for (auto const& handle : handles_to_read_variable_length) {
     BtaGattQueue::ReadCharacteristic(connection_id, handle, chrc_read_cb, nullptr);
   }
