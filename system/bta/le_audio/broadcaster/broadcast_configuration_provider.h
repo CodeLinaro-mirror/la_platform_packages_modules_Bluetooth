2fa87591d7e6d108362e56e6466154e07c9060c6
dongcao <dongcao@qti.qualcomm.com>
2025-06-25 16:17:16 +0530

    Correct Opensource QTI markings
    Change-Id: Ie4cb728820b39d5d2d1b4ef5a7e1964fbbb0e162

--- system/bta/le_audio/broadcaster/broadcast_configuration_provider.h
+++ system/bta/le_audio/broadcaster/broadcast_configuration_provider.h
@@ -15,7 +15,7 @@
  *
  * Changes from Qualcomm Technologies, Inc. are provided under the following license:
  *
- * Copyright (c) Qualcomm Technologies, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  *
  */
