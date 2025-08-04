990eeb50e5d7651f2774f16ff18f6da0884bc030
dongcao <dongcao@qti.qualcomm.com>
2025-08-04 12:43:33 +0530

    Correct Opensource QTI markings
    Change-Id: I7d9c396d1b4cd0a16afd29d909ed6e94d43381f5
    (cherry picked from commit 9523b312e206ca0f92482c750320f3ff3172ef46)

--- system/btif/src/btif_dm.cc
+++ system/btif/src/btif_dm.cc
@@ -15,9 +15,9 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *  Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ *  Changes from Qualcomm Technologies, Inc. are provided under the following license:
  *
- *  Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *  SPDX-License-Identifier: BSD-3-Clause-Clear.
  *
  ******************************************************************************************/
