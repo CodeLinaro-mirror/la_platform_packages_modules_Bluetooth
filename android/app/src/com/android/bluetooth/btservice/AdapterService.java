3fc03c86ab81ff65b14ad99bff7b9bcdaadc0506
Kuichu Ni <kuichun@qti.qualcomm.com>
2025-09-19 16:46:11 +0800

    Fix issue that cannot disconnect connection
    With car role, the Headset profile service (mHeadsetService) has not
    been created, so it leads to dereferences a null reference, causing
    the NPE and aborting the whole disconnect sequence.
    
    To fix it, adding a null‑check.
    
    Change-Id: I7f65d6ff4b5c158c37c2b646fa3827858cf8557c

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -3565,13 +3565,17 @@ public class AdapterService extends Service {
             mHeadsetClientService.disconnect(device);
         }
 
-        Log.d(TAG,"Call and Ringing Status are:"+mHeadsetService.isInCall() +" "
-             +mHeadsetService.isRinging());
+        if (mHeadsetService != null) {
+            Log.d(TAG,"Call and Ringing Status are:"+mHeadsetService.isInCall() +" "
+            + mHeadsetService.isRinging());
+        }
+
         Log.d(TAG,"Checking in A2DP Disconnect delay BL");
 
         //Adding A2DP Disconnect delay for blacklisted devices
         if (isDelayA2dpDiscDevice(device) &&
-                  (mHeadsetService.isInCall() || mHeadsetService.isRinging())) {
+                  ((mHeadsetService != null && mHeadsetService.isInCall())
+                      || (mHeadsetService != null && mHeadsetService.isRinging()))) {
            Log.e(TAG,"isDelayA2dpDiscDevice sleep 400ms");
               SystemClock.sleep(400);
         }
