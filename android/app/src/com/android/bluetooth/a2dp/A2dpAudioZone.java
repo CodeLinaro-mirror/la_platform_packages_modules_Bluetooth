d287237736b1280799051897b336293428ab4cca
Fei Zheng <zfei@qti.qualcomm.com>
2025-09-29 11:39:57 +0800

    A2DP: Restore audio zone operations and media key event dispatch
    For dual A2DP source:
    1. Audio‑zone set/get/clear functions through CarAudioManager are
    restored for Dual A2DP Source
    
    2. Update media key event dispatch to target the designated
    media player package
    
    Change-Id: I2852c76641ad7bec1e28a2888b5d27d4ed871d0a

--- android/app/src/com/android/bluetooth/a2dp/A2dpAudioZone.java
+++ android/app/src/com/android/bluetooth/a2dp/A2dpAudioZone.java
@@ -138,7 +138,7 @@ public final class A2dpAudioZone {
             if (carAudioManager == null) {
                 return false;
             }
-            return /*carAudioManager.setZoneIdForUid(zone, uid)*/true;
+            return carAudioManager.setZoneIdForUid(zone, uid);
         }
     }
     private boolean mapAudioZone(int zoneIndex) {
@@ -180,7 +180,7 @@ public final class A2dpAudioZone {
                     if (appInfo != null) {
                         CarAudioManager carAudioManager = getCarAudioManager(mContext);
                         if (carAudioManager != null) {
-                            //carAudioManager.clearZoneIdForUid(appInfo.uid);
+                            carAudioManager.clearZoneIdForUid(appInfo.uid);
                         }
                     }
                 }
