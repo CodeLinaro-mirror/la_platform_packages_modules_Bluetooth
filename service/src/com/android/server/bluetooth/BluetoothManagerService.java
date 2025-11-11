5be51f6be548fe0705eb3c8b577817f3b4bc9088
yingwu <yingwu@qti.qualcomm.com>
2025-11-11 08:59:38 +0530

    DUAL_BT: Fix the 2nd BT fails to enable sometimes
    Issue:
    The 2nd BT fails to enable sometimes.
    
    Root cause:
    (1) MESSAGE_DISABLE is sent with the timeout of 300ms,
        when to disable BT.
    
    (2) BT app may re-enable BT, before the timeout for
        MESSAGE_DISABLE is triggered.
    
    (3) This results into BT re-enable aborts when previous
        disable timeout occurs.
    
    Resolution:
    Clear MESSAGE_DISABLE and MESSAGE_HANDLE_DISABLE_DELAYED
    when BT disablement is completed.
    
    Change-Id: I1d48fc48eca94322240076513193c8c1a6aa5451

--- service/src/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -1240,6 +1240,8 @@ class BluetoothManagerService {
         Log.d(TAG, "unbindAndFinish(" + mAdapterIndex + "): mAdapter=" + mAdapter + " isBinding=" + isBinding());
 
         mHandler.removeMessages(MESSAGE_BLUETOOTH_STATE_CHANGE);
+        mHandler.removeMessages(MESSAGE_DISABLE);
+        mHandler.removeMessages(MESSAGE_HANDLE_DISABLE_DELAYED);
         if (mAdapter == null) {
             // mAdapter can be null when Bluetooth crashed and sent SERVICE_DISCONNECTED
             return;
