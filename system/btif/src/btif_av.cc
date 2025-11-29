4afaf2f1099310073deefc4892f4a36f9b5f7166
Yanfei Zhou <zyanfei@qti.qualcomm.com>
2025-11-29 17:21:41 +0530

    AVRCP: Get peer address by RC handle if active peer is invalid
    Issue found:
    Sometimes media file system can't be displayed in Bluetooth Media Player
    if peer device is the initiator of connection request.
    
    Root cause:
    Establishment of AVCTP_Browsing channel is prior to AVDTP streaming
    transitioning to 'OPEN' state, therefore, while handling meta message
    in btif layer, it assigns an empty address for the message due active
    peer is invalid at the moment. This results in btif layer ignoring the
    upstream meta messages.
    
    Resolution:
    Refine to get peer address by RC handle if active peer is invalid.
    
    CRs-Fixed: 4207482
    
    Change-Id: I72004a08fe4feaff5d607addc042cf8717573e61
    (cherry picked from commit 5ee3c71e26f417dfbe53fb4b813e90bf739bf4a3)

--- system/btif/src/btif_av.cc
+++ system/btif/src/btif_av.cc
@@ -3256,10 +3256,10 @@ static void btif_av_handle_bta_av_event(uint8_t peer_sep, const BtifAvEvent& bti
     case BTA_AV_VENDOR_CMD_EVT:
     case BTA_AV_VENDOR_RSP_EVT:
     case BTA_AV_META_MSG_EVT: {
+      const tBTA_AV_REMOTE_CMD& rc_rmt_cmd = p_data->remote_cmd;
+      btif_rc_get_addr_by_handle(rc_rmt_cmd.rc_handle, peer_address);
       if (btif_av_src_sink_coexist_enabled()) {
         if (peer_sep == AVDT_TSEP_INVALID) {
-          const tBTA_AV_REMOTE_CMD& rc_rmt_cmd = p_data->remote_cmd;
-          btif_rc_get_addr_by_handle(rc_rmt_cmd.rc_handle, peer_address);
           if (peer_address == RawAddress::kEmpty) {
             peer_address = btif_av_source.ActivePeer();
             if (peer_address == RawAddress::kEmpty) {
@@ -3281,9 +3281,12 @@ static void btif_av_handle_bta_av_event(uint8_t peer_sep, const BtifAvEvent& bti
       // events are received from the AVRCP module.
       if (peer_sep == AVDT_TSEP_SNK) {
         peer_address = btif_av_source.ActivePeer();
+        if (peer_address == RawAddress::kEmpty)
+          log::error("peer_address is empty");
         msg = "Stream sink offloaded";
       } else if (peer_sep == AVDT_TSEP_SRC) {
-        peer_address = btif_av_sink.ActivePeer();
+        if (peer_address == RawAddress::kEmpty)
+          peer_address = btif_av_sink.ActivePeer();
         msg = "Stream source offloaded";
       }
       break;
