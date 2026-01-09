miyu-xu/packages-modules-Bluetooth

Split sec_state into link-specific variables
Makes more sense when it's separate

Bug: 343639888
Test: establish LE and Classic connection to dual mode device, verify
      encryption and profiles are connected on both transports
Change-Id: I0de9075278010f36a332156a8d984efba08fac00

system/stack/btm/btm_ble_sec.cc
@@ -1067,7 +1067,7 @@ void btm_ble_link_sec_check(const RawAddress& bd_addr,
   }
 
   if (p_dev_rec->sec_rec.is_security_state_encrypting() ||
-      p_dev_rec->sec_rec.sec_state == tSECURITY_STATE::BTM_SEC_STATE_AUTHENTICATING) {
+      p_dev_rec->sec_rec.le_link == tSECURITY_STATE::BTM_SEC_STATE_AUTHENTICATING) {
     /* race condition: discard the security request while central is encrypting
      * the link */
     *p_sec_req_act = BTM_BLE_SEC_REQ_ACT_DISCARD;
@@ -1171,7 +1171,7 @@ tBTM_STATUS btm_ble_set_encryption(const RawAddress& bd_addr,
 
       if (SMP_Pair(bd_addr) == SMP_STARTED) {
         cmd = BTM_CMD_STARTED;
-        p_rec->sec_rec.sec_state = tSECURITY_STATE::BTM_SEC_STATE_AUTHENTICATING;
+        p_rec->sec_rec.le_link = tSECURITY_STATE::BTM_SEC_STATE_AUTHENTICATING;
       }
       break;
 
@@ -1252,8 +1252,8 @@ tBTM_STATUS btm_ble_start_encrypt(const RawAddress& bda, bool use_stk,
     return BTM_ERR_KEY_MISSING;
   }
 
-  if (p_rec->sec_rec.sec_state == tSECURITY_STATE::BTM_SEC_STATE_IDLE) {
-    p_rec->sec_rec.sec_state = tSECURITY_STATE::BTM_SEC_STATE_LE_ENCRYPTING;
+  if (p_rec->sec_rec.le_link == tSECURITY_STATE::BTM_SEC_STATE_IDLE) {
+    p_rec->sec_rec.le_link = tSECURITY_STATE::BTM_SEC_STATE_ENCRYPTING;
   }
 
   return BTM_CMD_STARTED;
@@ -1324,7 +1324,7 @@ void btm_ble_link_encrypted(const RawAddress& bd_addr, uint8_t encr_enable) {
   if (encr_enable && p_dev_rec->sec_rec.enc_key_size == 0)
     p_dev_rec->sec_rec.enc_key_size = p_dev_rec->sec_rec.ble_keys.key_size;
 
-  p_dev_rec->sec_rec.sec_state = tSECURITY_STATE::BTM_SEC_STATE_IDLE;
+  p_dev_rec->sec_rec.le_link = tSECURITY_STATE::BTM_SEC_STATE_IDLE;
   if (p_dev_rec->sec_rec.p_callback && enc_cback) {
     if (encr_enable) btm_sec_dev_rec_cback_event(p_dev_rec, BTM_SUCCESS, true);
     /* LTK missing on peripheral */
@@ -1594,7 +1594,7 @@ tBTM_STATUS btm_proc_smp_cback(tSMP_EVT event, const RawAddress& bd_addr,
         }
         btm_sec_cb.pairing_bda = bd_addr;
         if (event != SMP_CONSENT_REQ_EVT) {
-          p_dev_rec->sec_rec.sec_state = tSECURITY_STATE::BTM_SEC_STATE_AUTHENTICATING;
+          p_dev_rec->sec_rec.le_link = tSECURITY_STATE::BTM_SEC_STATE_AUTHENTICATING;
         }
         btm_sec_cb.pairing_flags |= BTM_PAIR_FLAGS_LE_ACTIVE;
         FALLTHROUGH_INTENDED; /* FALLTHROUGH */
@@ -1651,7 +1651,7 @@ tBTM_STATUS btm_proc_smp_cback(tSMP_EVT event, const RawAddress& bd_addr,
           }
 
           if (res == BTM_SUCCESS) {
-            p_dev_rec->sec_rec.sec_state = tSECURITY_STATE::BTM_SEC_STATE_IDLE;
+            p_dev_rec->sec_rec.le_link = tSECURITY_STATE::BTM_SEC_STATE_IDLE;
 
             if (p_dev_rec->sec_rec.bond_type != BOND_TYPE_TEMPORARY) {
               // Add all bonded device into resolving list if IRK is available.

system/stack/btm/btm_dev.cc
@@ -214,7 +214,8 @@ void BTM_SecClearSecurityFlags(const RawAddress& bd_addr) {
   if (p_dev_rec == NULL) return;
 
   p_dev_rec->sec_rec.sec_flags = 0;
-  p_dev_rec->sec_rec.sec_state = tSECURITY_STATE::BTM_SEC_STATE_IDLE;
+  p_dev_rec->sec_rec.le_link = tSECURITY_STATE::BTM_SEC_STATE_IDLE;
+  p_dev_rec->sec_rec.classic_link = tSECURITY_STATE::BTM_SEC_STATE_IDLE;
   p_dev_rec->sm4 = BTM_SM4_UNKNOWN;
 }
 

system/stack/btm/btm_devctl.cc
@@ -163,7 +163,8 @@ void BTM_db_reset(void) {
 
 static bool set_sec_state_idle(void* data, void* /* context */) {
   tBTM_SEC_DEV_REC* p_dev_rec = static_cast<tBTM_SEC_DEV_REC*>(data);
-  p_dev_rec->sec_rec.sec_state = tSECURITY_STATE::BTM_SEC_STATE_IDLE;
+  p_dev_rec->sec_rec.le_link = tSECURITY_STATE::BTM_SEC_STATE_IDLE;
+  p_dev_rec->sec_rec.classic_link = tSECURITY_STATE::BTM_SEC_STATE_IDLE;
   return true;
 }
 

system/stack/btm/security_device_record.h
@@ -166,9 +166,7 @@ enum class tSECURITY_STATE : uint8_t {
   /* delay to check for encryption to work around */
   /* controller problems */
   BTM_SEC_STATE_DELAY_FOR_ENC = 7,
-  BTM_SEC_STATE_DISCONNECTING_BLE = 8,
-  BTM_SEC_STATE_DISCONNECTING_BOTH = 9,
-  BTM_SEC_STATE_LE_ENCRYPTING = 10,
+
 };
 
 static inline std::string security_state_text(const tSECURITY_STATE& state) {
@@ -181,9 +179,6 @@ static inline std::string security_state_text(const tSECURITY_STATE& state) {
     CASE_RETURN_STRING(tSECURITY_STATE::BTM_SEC_STATE_SWITCHING_ROLE);
     CASE_RETURN_STRING(tSECURITY_STATE::BTM_SEC_STATE_DISCONNECTING);
     CASE_RETURN_STRING(tSECURITY_STATE::BTM_SEC_STATE_DELAY_FOR_ENC);
-    CASE_RETURN_STRING(tSECURITY_STATE::BTM_SEC_STATE_DISCONNECTING_BLE);
-    CASE_RETURN_STRING(tSECURITY_STATE::BTM_SEC_STATE_DISCONNECTING_BOTH);
-    CASE_RETURN_STRING(tSECURITY_STATE::BTM_SEC_STATE_LE_ENCRYPTING);
     default:
       RETURN_UNKNOWN_TYPE_STRING(tSECURITY_STATE, state);
   }
@@ -208,7 +203,9 @@ typedef enum : uint8_t {
  * A record exists for each device authenticated with this device
  */
 struct tBTM_SEC_REC {
-  tSECURITY_STATE sec_state; /* Operating state                    */
+  // tSECURITY_STATE sec_state; /* Operating state */
+  tSECURITY_STATE classic_link; /* Operating state of Classic link */
+  tSECURITY_STATE le_link;      /* Operating state of LE link */
 
   tHCI_STATUS sec_status; /* Status in encryption change event  */
   uint16_t sec_flags;     /* Current device security state      */
@@ -304,40 +301,18 @@ struct tBTM_SEC_REC {
     sec_flags &= ~BTM_SEC_16_DIGIT_PIN_AUTHED;
   }
 
-  bool is_security_state_idle() const { return sec_state == tSECURITY_STATE::BTM_SEC_STATE_IDLE; }
-  bool is_security_state_authenticating() const {
-    return sec_state == tSECURITY_STATE::BTM_SEC_STATE_AUTHENTICATING;
-  }
   bool is_security_state_bredr_encrypting() const {
-    return sec_state == tSECURITY_STATE::BTM_SEC_STATE_ENCRYPTING;
+    return classic_link == tSECURITY_STATE::BTM_SEC_STATE_ENCRYPTING;
   }
   bool is_security_state_le_encrypting() const {
-    return sec_state == tSECURITY_STATE::BTM_SEC_STATE_LE_ENCRYPTING;
+    return le_link == tSECURITY_STATE::BTM_SEC_STATE_ENCRYPTING;
   }
   bool is_security_state_encrypting() const {
     return (is_security_state_bredr_encrypting() ||
             is_security_state_le_encrypting());
   }
   bool is_security_state_getting_name() const {
-    return sec_state == tSECURITY_STATE::BTM_SEC_STATE_GETTING_NAME;
-  }
-  bool is_security_state_authorizing() const {
-    return sec_state == tSECURITY_STATE::BTM_SEC_STATE_AUTHORIZING;
-  }
-  bool is_security_state_switching_role() const {
-    return sec_state == tSECURITY_STATE::BTM_SEC_STATE_SWITCHING_ROLE;
-  }
-  bool is_security_state_disconnecting() const {
-    return sec_state == tSECURITY_STATE::BTM_SEC_STATE_DISCONNECTING;
-  }
-  bool is_security_state_wait_for_encryption() const {
-    return sec_state == tSECURITY_STATE::BTM_SEC_STATE_DELAY_FOR_ENC;
-  }
-  bool is_security_state_ble_disconnecting() const {
-    return sec_state == tSECURITY_STATE::BTM_SEC_STATE_DISCONNECTING_BLE;
-  }
-  bool is_security_state_br_edr_and_ble() const {
-    return sec_state == tSECURITY_STATE::BTM_SEC_STATE_DISCONNECTING_BOTH;
+    return classic_link == tSECURITY_STATE::BTM_SEC_STATE_GETTING_NAME;
   }
 
   bool is_bond_type_unknown() const { return bond_type == BOND_TYPE_UNKNOWN; }

CodeLinaro-mirror/la_platform_packages_modules_Bluetooth

Currently when both LE and Classic links are established at same time,
and both are encrypted at same time, we have a problem. Same variable is
used to mark state as "encrypting" for both links, and it can cause race
conditions.
To fix that, keep security state as separate variable for LE and Classic
link.
This patch also removes security states that were added to address
similar issues around disconnection.
Test: m -j32;
Flag: exempt, can come up with good way to flag this without
      significantly obfruscating the code
CRs-Fixed: 4371930
(cherry picked from commit 98d45b6a21ec8cd2346bf17e50a2cf75b3d2b130)

system/stack/btm/btm_ble_sec.cc
@@ -1083,7 +1083,7 @@ void btm_ble_link_sec_check(const RawAddress& bd_addr,
   }
 
   if (p_dev_rec->sec_rec.is_security_state_encrypting() ||
-      p_dev_rec->sec_rec.sec_state == tSECURITY_STATE::AUTHENTICATING) {
+      p_dev_rec->sec_rec.le_link == tSECURITY_STATE::AUTHENTICATING) {
     /* race condition: discard the security request while central is encrypting
      * the link */
     *p_sec_req_act = BTM_BLE_SEC_REQ_ACT_DISCARD;
@@ -1191,7 +1191,7 @@ tBTM_STATUS btm_ble_set_encryption(const RawAddress& bd_addr,
 
       if (SMP_Pair(bd_addr) == SMP_STARTED) {
         cmd = BTM_CMD_STARTED;
-        p_rec->sec_rec.sec_state = tSECURITY_STATE::AUTHENTICATING;
+        p_rec->sec_rec.le_link = tSECURITY_STATE::AUTHENTICATING;
       }
       break;
 
@@ -1308,8 +1308,8 @@ tBTM_STATUS btm_ble_start_encrypt(const RawAddress& bda, bool use_stk,
     return BTM_ERR_KEY_MISSING;
   }
 
-  if (p_rec->sec_rec.sec_state == tSECURITY_STATE::IDLE) {
-    p_rec->sec_rec.sec_state = tSECURITY_STATE::LE_ENCRYPTING;
+  if (p_rec->sec_rec.le_link == tSECURITY_STATE::IDLE) {
+    p_rec->sec_rec.le_link = tSECURITY_STATE::ENCRYPTING;
   }
 
   return BTM_CMD_STARTED;
@@ -1380,7 +1380,7 @@ void btm_ble_link_encrypted(const RawAddress& bd_addr, uint8_t encr_enable) {
   if (encr_enable && p_dev_rec->sec_rec.enc_key_size == 0)
     p_dev_rec->sec_rec.enc_key_size = p_dev_rec->sec_rec.ble_keys.key_size;
 
-  p_dev_rec->sec_rec.sec_state = tSECURITY_STATE::IDLE;
+  p_dev_rec->sec_rec.le_link = tSECURITY_STATE::IDLE;
   if (p_dev_rec->sec_rec.p_callback && enc_cback) {
     if (encr_enable) btm_sec_dev_rec_cback_event(p_dev_rec, BTM_SUCCESS, true);
     /* LTK missing on peripheral */
@@ -1699,7 +1699,7 @@ tBTM_STATUS btm_proc_smp_cback(tSMP_EVT event, const RawAddress& bd_addr,
         }
         btm_sec_cb.pairing_bda = bd_addr;
         if (event != SMP_CONSENT_REQ_EVT) {
-          p_dev_rec->sec_rec.sec_state = tSECURITY_STATE::AUTHENTICATING;
+          p_dev_rec->sec_rec.le_link = tSECURITY_STATE::AUTHENTICATING;
         }
         btm_sec_cb.pairing_flags |= BTM_PAIR_FLAGS_LE_ACTIVE;
         FALLTHROUGH_INTENDED; /* FALLTHROUGH */
@@ -1765,7 +1765,7 @@ tBTM_STATUS btm_proc_smp_cback(tSMP_EVT event, const RawAddress& bd_addr,
           }
 
           if (res == BTM_SUCCESS) {
-            p_dev_rec->sec_rec.sec_state = tSECURITY_STATE::IDLE;
+            p_dev_rec->sec_rec.le_link = tSECURITY_STATE::IDLE;
 
             if (p_dev_rec->sec_rec.bond_type != BOND_TYPE_TEMPORARY) {
               // Add all bonded device into resolving list if IRK is available.

system/stack/btm/btm_dev.cc
@@ -215,7 +215,8 @@ void BTM_SecClearSecurityFlags(const RawAddress& bd_addr) {
   if (p_dev_rec == NULL) return;
 
   p_dev_rec->sec_rec.sec_flags = 0;
-  p_dev_rec->sec_rec.sec_state = tSECURITY_STATE::IDLE;
+  p_dev_rec->sec_rec.le_link = tSECURITY_STATE::IDLE;
+  p_dev_rec->sec_rec.classic_link = tSECURITY_STATE::IDLE;
   p_dev_rec->sm4 = BTM_SM4_UNKNOWN;
 }
 

system/stack/btm/btm_devctl.cc
@@ -161,7 +161,8 @@ void BTM_db_reset(void) {
 
 static bool set_sec_state_idle(void* data, void* /* context */) {
   tBTM_SEC_DEV_REC* p_dev_rec = static_cast<tBTM_SEC_DEV_REC*>(data);
-  p_dev_rec->sec_rec.sec_state = tSECURITY_STATE::IDLE;
+  p_dev_rec->sec_rec.le_link = tSECURITY_STATE::IDLE;
+  p_dev_rec->sec_rec.classic_link = tSECURITY_STATE::IDLE;
   return true;
 }
 

system/stack/btm/security_device_record.h
@@ -166,9 +166,6 @@ enum class tSECURITY_STATE : uint8_t {
   /* delay to check for encryption to work around */
   /* controller problems */
   DELAY_FOR_ENC = 7,
-  DISCONNECTING_BLE = 8,
-  DISCONNECTING_BOTH = 9,
-  LE_ENCRYPTING = 10,
 };
 
 static inline std::string security_state_text(const tSECURITY_STATE& state) {
@@ -181,9 +178,6 @@ static inline std::string security_state_text(const tSECURITY_STATE& state) {
     CASE_RETURN_STRING(tSECURITY_STATE::SWITCHING_ROLE);
     CASE_RETURN_STRING(tSECURITY_STATE::DISCONNECTING);
     CASE_RETURN_STRING(tSECURITY_STATE::DELAY_FOR_ENC);
-    CASE_RETURN_STRING(tSECURITY_STATE::DISCONNECTING_BLE);
-    CASE_RETURN_STRING(tSECURITY_STATE::DISCONNECTING_BOTH);
-    CASE_RETURN_STRING(tSECURITY_STATE::LE_ENCRYPTING);
     default:
       RETURN_UNKNOWN_TYPE_STRING(tSECURITY_STATE, state);
   }
@@ -208,7 +202,8 @@ typedef enum : uint8_t {
  * A record exists for each device authenticated with this device
  */
 struct tBTM_SEC_REC {
-  tSECURITY_STATE sec_state; /* Operating state                    */
+  tSECURITY_STATE classic_link; /* Operating state of Classic link */
+  tSECURITY_STATE le_link;      /* Operating state of LE link */
 
   tHCI_STATUS sec_status; /* Status in encryption change event  */
   uint16_t sec_flags;     /* Current device security state      */
@@ -304,36 +299,16 @@ struct tBTM_SEC_REC {
     sec_flags &= ~BTM_SEC_16_DIGIT_PIN_AUTHED;
   }
 
-  bool is_security_state_idle() const { return sec_state == tSECURITY_STATE::IDLE; }
-  bool is_security_state_authenticating() const {
-    return sec_state == tSECURITY_STATE::AUTHENTICATING;
-  }
   bool is_security_state_bredr_encrypting() const {
-    return sec_state == tSECURITY_STATE::ENCRYPTING;
+    return classic_link == tSECURITY_STATE::ENCRYPTING;
   }
-  bool is_security_state_le_encrypting() const {
-    return sec_state == tSECURITY_STATE::LE_ENCRYPTING;
-  }
+  bool is_security_state_le_encrypting() const { return le_link == tSECURITY_STATE::ENCRYPTING; }
   bool is_security_state_encrypting() const {
     return (is_security_state_bredr_encrypting() ||
             is_security_state_le_encrypting());
   }
-  bool is_security_state_getting_name() const { return sec_state == tSECURITY_STATE::GETTING_NAME; }
-  bool is_security_state_authorizing() const { return sec_state == tSECURITY_STATE::AUTHORIZING; }
-  bool is_security_state_switching_role() const {
-    return sec_state == tSECURITY_STATE::SWITCHING_ROLE;
-  }
-  bool is_security_state_disconnecting() const {
-    return sec_state == tSECURITY_STATE::DISCONNECTING;
-  }
-  bool is_security_state_wait_for_encryption() const {
-    return sec_state == tSECURITY_STATE::DELAY_FOR_ENC;
-  }
-  bool is_security_state_ble_disconnecting() const {
-    return sec_state == tSECURITY_STATE::DISCONNECTING_BLE;
-  }
-  bool is_security_state_br_edr_and_ble() const {
-    return sec_state == tSECURITY_STATE::DISCONNECTING_BOTH;
+  bool is_security_state_getting_name() const {
+    return classic_link == tSECURITY_STATE::GETTING_NAME;
   }
 
   bool is_bond_type_unknown() const { return bond_type == BOND_TYPE_UNKNOWN; }