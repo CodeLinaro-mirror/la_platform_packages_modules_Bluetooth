fdd7fe58a9bdee42d028739bec2ee9c9c783e9a7
kaidshi <kaidshi@qti.qualcomm.com>
2026-02-06 16:47:19 +0800

    Fix register gatt service always failed issue.
    Change-Id: I8095b4270e63515a620852dcd5b57b0bb7d8b992

--- system/certification_tools/blegatt_test/gatt_test.cpp
+++ system/certification_tools/blegatt_test/gatt_test.cpp
@@ -3139,11 +3139,11 @@ void do_le_server_add_service(char* p) {
 void do_le_server_add_custom_service(char* p) {
   int Ret = 0;
   bool is_valid = false;
-  int uuid_len = 0, uuid_len_bytes = 0;
+  int uuid_len = 0;
 
   uuid_len = get_int(&p, -1);  // arg1 - Size in bytes for the uuid (2, 4, or
                                // 16)
-  std::string uuid_str = get_uuid_str(&p, uuid_len_bytes);  // arg2 service uuid
+  std::string uuid_str = get_uuid_str(&p, uuid_len);  // arg2 service uuid
   printf("%s:: uuid_str=%s \n", __FUNCTION__, uuid_str.c_str());
 
   std::vector<btgatt_db_element_t> service;
