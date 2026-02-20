msft-mirror-aosp/platform.packages.modules.Bluetooth

Backport regression fixes for 288144143
These are backports for the following AOSP changes:
aosp/3424923
aosp/3420039
aosp/3394039

which address regressions in the initial fix for above bug.

Bug: 288144143
Test: m libbluetooth
Ignore-AOSP-First: security
Tag: #security
Merged-In: Ibcf0055242970f5354c6127ce908171f89b2b88e
Merged-In: I877223e4a7141c73bc28ae2133fd26f4bd736321
Merged-In: I78a249561fd8ccab38e32fe2e14d90e3b4420dcb
Change-Id: I78a249561fd8ccab38e32fe2e14d90e3b4420dcb

system/stack/btm/btm_ble.cc
@@ -1417,6 +1417,9 @@ tBTM_STATUS btm_ble_set_encryption(const RawAddress& bd_addr,
   switch (sec_act) {
     case BTM_BLE_SEC_ENCRYPT:
       if (link_role == HCI_ROLE_CENTRAL) {
+        if (p_rec->is_le_device_encrypted()) {
+          return BTM_SUCCESS;
+        }
         /* start link layer encryption using the security info stored */
         cmd = btm_ble_start_encrypt(bd_addr, false, NULL);
         break;
@@ -1771,6 +1774,35 @@ void btm_ble_connected_from_address_with_type(
                     address_with_type.type, addr_matched);
 }
 
+static bool btm_ble_complete_evt_ignore(const tBTM_SEC_DEV_REC* p_dev_rec,
+                                        const tSMP_EVT_DATA* p_data) {
+  // Encryption request in peripheral role results in SMP Security request. SMP
+  // may generate a SMP_COMPLT_EVT failure event cases like below: 1) Some
+  // central devices don't handle cross-over between encryption and SMP security
+  // request 2) Link may get disconnected after the SMP security request was
+  // sent.
+  if (p_data->cmplt.reason != SMP_SUCCESS && !p_dev_rec->role_central &&
+      btm_cb.pairing_bda != p_dev_rec->bd_addr &&
+      btm_cb.pairing_bda != p_dev_rec->ble.pseudo_addr &&
+      p_dev_rec->is_le_link_key_known() &&
+      p_dev_rec->ble.key_type != BTM_LE_KEY_NONE) {
+    if (p_dev_rec->is_le_device_encrypted()) {
+      LOG_WARN("Bonded device %s is already encrypted, ignoring SMP failure",
+               PRIVATE_ADDRESS(p_dev_rec->bd_addr));
+      return true;
+    } else if (p_data->cmplt.reason == SMP_CONN_TOUT) {
+      LOG_WARN(
+          "Bonded device %s disconnected while waiting for encryption, "
+          "ignoring SMP failure",
+          PRIVATE_ADDRESS(p_dev_rec->bd_addr));
+      l2cu_start_post_bond_timer(p_dev_rec->ble_hci_handle);
+      return true;
+    }
+  }
+
+  return false;
+}
+
 /*****************************************************************************
  *  Function        btm_proc_smp_cback
  *
@@ -1823,6 +1855,10 @@ tBTM_STATUS btm_proc_smp_cback(tSMP_EVT event, const RawAddress& bd_addr,
         FALLTHROUGH_INTENDED; /* FALLTHROUGH */
 
       case SMP_COMPLT_EVT:
+        if (btm_ble_complete_evt_ignore(p_dev_rec, p_data)) {
+          return BTM_SUCCESS;
+        }
+
         if (btm_cb.api.p_le_callback) {
           /* the callback function implementation may change the IO
            * capability... */

CodeLinaro-mirror/la_platform_packages_modules_Bluetooth

system/stack/btm/btm_ble.cc
@@ -1373,6 +1373,9 @@ tBTM_STATUS btm_ble_set_encryption(const RawAddress& bd_addr,
   switch (sec_act) {
     case BTM_BLE_SEC_ENCRYPT:
       if (link_role == HCI_ROLE_CENTRAL) {
+        if (p_rec->is_le_device_encrypted()) {
+          return BTM_SUCCESS;
+        }
         /* start link layer encryption using the security info stored */
         cmd = btm_ble_start_encrypt(bd_addr, false, NULL);
         break;
@@ -1753,6 +1756,35 @@ void btm_ble_connected(const RawAddress& bda, uint16_t handle, uint8_t enc_mode,
   btm_cb.ble_ctr_cb.inq_var.directed_conn = BTM_BLE_ADV_IND_EVT;
 }
 
+static bool btm_ble_complete_evt_ignore(const tBTM_SEC_DEV_REC* p_dev_rec,
+                                        const tSMP_EVT_DATA* p_data) {
+  // Encryption request in peripheral role results in SMP Security request. SMP
+  // may generate a SMP_COMPLT_EVT failure event cases like below: 1) Some
+  // central devices don't handle cross-over between encryption and SMP security
+  // request 2) Link may get disconnected after the SMP security request was
+  // sent.
+  if (p_data->cmplt.reason != SMP_SUCCESS && !p_dev_rec->role_central &&
+      btm_cb.pairing_bda != p_dev_rec->bd_addr &&
+      btm_cb.pairing_bda != p_dev_rec->ble.pseudo_addr &&
+      p_dev_rec->is_le_link_key_known() &&
+      p_dev_rec->ble.key_type != BTM_LE_KEY_NONE) {
+    if (p_dev_rec->is_le_device_encrypted()) {
+      LOG_WARN("Bonded device %s is already encrypted, ignoring SMP failure",
+               ADDRESS_TO_LOGGABLE_CSTR(p_dev_rec->bd_addr));
+      return true;
+    } else if (p_data->cmplt.reason == SMP_CONN_TOUT) {
+      LOG_WARN(
+          "Bonded device %s disconnected while waiting for encryption, "
+          "ignoring SMP failure",
+          ADDRESS_TO_LOGGABLE_CSTR(p_dev_rec->bd_addr));
+      l2cu_start_post_bond_timer(p_dev_rec->ble_hci_handle);
+      return true;
+    }
+  }
+
+  return false;
+}
+
 /*****************************************************************************
  *  Function        btm_proc_smp_cback
  *
@@ -1805,6 +1837,10 @@ tBTM_STATUS btm_proc_smp_cback(tSMP_EVT event, const RawAddress& bd_addr,
         FALLTHROUGH_INTENDED; /* FALLTHROUGH */
 
       case SMP_COMPLT_EVT:
+        if (btm_ble_complete_evt_ignore(p_dev_rec, p_data)) {
+          return BTM_SUCCESS;
+        }
+
         if (btm_cb.api.p_le_callback) {
           /* the callback function implementation may change the IO
            * capability... */