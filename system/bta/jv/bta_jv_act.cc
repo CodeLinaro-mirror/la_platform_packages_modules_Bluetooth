17765b198488f3aa1daa1634017f437bff977ac8
cxin <cxin@qti.qualcomm.com>
2026-03-02 17:44:53 +0800

    Fix ots channel connection timeout issue due to btif send redundant close command.
    RCA: When remote device closes L2CAP, btif calls BTA_JvL2capClose() and the actual action bta_jv_l2cap_close() is put in main thread and executed later. Since the state of bta_jv_cb.l2c_cb[gap_handle] has been set to NONE, it maybe reused by another new L2CAP connection, which sets the state to OPENING. Then the bta_jv_l2cap_close gets executed and find the state of bta_jv_cb.l2c_cb[handle] is not NONE, it calls GAP_ConnClose and closes the newly created L2CAP connection. Totally speaking, the key point is that he stack performs cleanup asynchronously, splitting the close operation into two phases, but the existing flag cannot prevent another ACL link from reusing  the L2cap connection to be closed in the middle.
    
    Fix: when the flag async is true, which means remote device closes L2CAP, do not call BTA_JvL2capClose(). Besides, do the clean up when receiving GAP_EVT_CONN_CLOSED.
    
    Change-Id: I1523e4a052e5a78222b95bd62c13576651ba2372

--- system/bta/jv/bta_jv_act.cc
+++ system/bta/jv/bta_jv_act.cc
@@ -1027,6 +1027,13 @@ static void bta_jv_l2cap_client_cback(uint16_t gap_handle, uint16_t event, tGAP_
                                                   : BTA_JV_L2CAP_REASON_EMPTY;
       p_cb->p_cback(BTA_JV_L2CAP_CLOSE_EVT, &evt_data, p_cb->l2cap_socket_id);
       p_cb->p_cback = NULL;
+#ifdef TARGET_QCOM_IOT_BT_EXT
+      bta_jv_free_set_pm_profile_cb((uint32_t)p_cb->handle);
+      p_cb->psm = 0;
+      p_cb->cong = false;
+      p_cb->handle = 0;
+      p_cb->l2cap_socket_id = 0;
+#endif
       break;
 
     case GAP_EVT_CONN_DATA_AVAIL:
