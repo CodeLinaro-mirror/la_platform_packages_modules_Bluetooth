ba9960d233ce95c93bad7b706b9b54f0c3830bb3
Brian Delwiche <delwiche@google.com>
2026-04-15 17:00:24 -0700

    Reject unsafe LE encryption key sizes
    Encryption key sizes smaller than 7 octet are forbidden by the Core specification. Minimum encryption key size is enforced for BR/EDR but not for LE.
    This change ensures that SMP also enforces the minimum key size security requirement.
    
    Test: mmm packages/modules/Bluetooth
    Flag: EXEMPT BUGFIX
    Bug: 483126552
    Change-Id: Id8801344217de992db6dd306ee975dd55992d505
    (cherry picked from commit 55c96018e4b47ef96f52d8a5696922e406228051)

--- system/stack/btm/btm_sec.cc
+++ system/stack/btm/btm_sec.cc
@@ -3471,9 +3471,6 @@ void btm_sec_encrypt_change(uint16_t handle, tHCI_STATUS status, uint8_t encr_en
     btm_sec_dev_rec_cback_event(p_dev_rec, btm_status, false);
   }
 }
-
-constexpr uint8_t MIN_KEY_SIZE = 7;
-
 static void read_encryption_key_size_complete_after_encryption_change(uint8_t status,
                                                                       uint16_t handle,
                                                                       uint8_t key_size) {
@@ -3493,7 +3490,7 @@ static void read_encryption_key_size_complete_after_encryption_change(uint8_t st
     return;
   }
 
-  if (key_size < MIN_KEY_SIZE) {
+  if (key_size < btm_sec_get_min_enc_key_size()) {
     log::error("encryption key too short, disconnecting. handle:0x{:x},key_size:{}", handle,
                key_size);
 
@@ -4068,7 +4065,7 @@ static void read_encryption_key_size_complete_after_key_refresh(uint8_t status, 
     return;
   }
 
-  if (key_size < MIN_KEY_SIZE) {
+  if (key_size < btm_sec_get_min_enc_key_size()) {
     log::error("encryption key too short, disconnecting. handle: 0x{:x} key_size {}", handle,
                key_size);
 
@@ -5238,4 +5235,9 @@ void BTM_update_version_info(const RawAddress& bd_addr,
   }
 
   p_dev_rec->remote_version_info = remote_version_info;
+}
+
+uint8_t btm_sec_get_min_enc_key_size() {
+  static uint8_t min_key_size = MIN_KEY_SIZE;
+  return min_key_size;
 }
