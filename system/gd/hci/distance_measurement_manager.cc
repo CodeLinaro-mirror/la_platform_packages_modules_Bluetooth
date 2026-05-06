52afb06235ecca1ba5aace3067e197b90e3d3c8c
Venkata Jagadeesh Garaga <quic_vgaraga@quicinc.com>
2026-05-06 15:48:43 +0530

    BCS: use ANTENNA_1 instead of ANTENNA_2 for Mode1
    Mode1 primarily uses Chain 0, so select ANTENNA_1
    to align the antenna configuration with the active chain
    for calbrition
    
    CRs-Fixed:  4607175
    
    Change-Id: Ib32884ebe38fe9b7ee2c91c7d0150e07f269561a

--- system/gd/hci/distance_measurement_manager.cc
+++ system/gd/hci/distance_measurement_manager.cc
@@ -70,7 +70,7 @@ static constexpr uint8_t kTxPowerNotAvailable = 0xfe;
 static constexpr int8_t kRSSIDropOffAt1M = 41;
 static constexpr uint8_t kCsMaxTxPower = 20;  // 10 dBm
 static constexpr CsSyncAntennaSelection kCsSyncAntennaSelection =
-         CsSyncAntennaSelection::ANTENNAS_IN_ORDER;
+         CsSyncAntennaSelection::ANTENNA_1;
 static constexpr uint8_t kConfigId = 0x01;  // Use 0x01 to create config and enable procedure
 static constexpr uint8_t kMinMainModeSteps = 0x02;
 static constexpr uint8_t kMaxMainModeSteps = 0x05;
