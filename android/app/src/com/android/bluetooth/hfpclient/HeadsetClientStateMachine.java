02a13d0534164cb8c5e3a3f3d738b9f718681604
xueyzhao <xueyzhao@qti.qualcomm.com>
2026-05-27 11:37:33 +0800

    HF_Client: Handle error responses in EVENT_TYPE_CMD_RESULT
    Issue:
    Calls are incorrectly removed occasionally.
    
    Root Cause:
    queryCallsDone() is invoked even though received +CME ERROR or other error codes.
    
    Resolution:
    Add explicit check for event.valueInt to differentiate OK vs error.
    If error, notify upper layer and skip queryCallsDone().
    
    Change-Id: I04d80f3c9459ca1723c0f561ec24bd7bfd720e81

--- android/app/src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
+++ android/app/src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
@@ -1707,6 +1707,7 @@ public class HeadsetClientStateMachine extends StateMachine {
                                 }
                                 mService.sendBroadcast(
                                         intent, BLUETOOTH_CONNECT, Utils.getTempBroadcastBundle());
+                                break;
                             }
 
                             switch (queuedAction.first) {
