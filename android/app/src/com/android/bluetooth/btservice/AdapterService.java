6a9631464407f09fcc43dbc8085caf410362aa48
Srinu Jella <sjella@qti.qualcomm.com>
2026-06-10 16:48:46 +0530

    Guard mHeadsetService null check in disconnectAllEnabledProfiles
    Root Cause: mHeadsetService could be null when
    disconnectAllEnabledProfiles is called, but it
    was accessed directly without null checks.
    
    Fix: Introduced null-guarded boolean locals
    inCall and ringing before use.
    
    Description: Prevents a potential NullPointerException
    in disconnectAllEnabledProfiles when mHeadsetService
    is null.
    
    CRs-Fixed: 4567595
    Change-Id: Ifd90e0800390eb4b32b6b288061be800736a3904

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -3586,13 +3586,15 @@ public class AdapterService extends Service {
             mHeadsetClientService.disconnect(device);
         }
 
-        Log.d(TAG,"Call and Ringing Status are:"+mHeadsetService.isInCall() +" "
-             +mHeadsetService.isRinging());
-        Log.d(TAG,"Checking in A2DP Disconnect delay BL");
+        boolean inCall = mHeadsetService != null
+                && mHeadsetService.isInCall();
+        boolean ringing = mHeadsetService != null
+                && mHeadsetService.isRinging();
+        Log.d(TAG, "Call and Ringing Status are:" + inCall + " " + ringing);
+        Log.d(TAG, "Checking in A2DP Disconnect delay BL");
 
         //Adding A2DP Disconnect delay for blacklisted devices
-        if (isDelayA2dpDiscDevice(device) &&
-                  (mHeadsetService.isInCall() || mHeadsetService.isRinging())) {
+        if (isDelayA2dpDiscDevice(device) && (inCall || ringing)) {
            Log.e(TAG,"isDelayA2dpDiscDevice sleep 400ms");
               SystemClock.sleep(400);
         }
