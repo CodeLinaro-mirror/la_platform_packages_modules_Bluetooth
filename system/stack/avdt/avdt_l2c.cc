53ae5a2c228ec0fff4c7e06b5bc5999a672c815b
Rajat Soni <quic_rajasoni@quicinc.com>
2026-07-14 12:03:21 +0530

    AVDT: Wait for L2CAP disconnect confirmation before closing channel
    avdt_ad_close_req() called avdt_ad_tc_close_ind() immediately after
    issuing L2CA_DisconnectReq(), acknowledging the transport channel
    close to the upper layer before L2CAP had actually confirmed the
    disconnection. This let the upper layer turn around and request a
    new connection while the old L2CAP channel teardown was still in
    flight, sending a connect request before the peer's disconnect was
    confirmed.
    
    Add L2CAP disconnect confirmation callback (avdt_l2c_disconnect_cfm_cback)
    and, under the avdt_wait_for_l2c_disconnect_cfm_before_ack_upper_layer
    flag, defer avdt_ad_tc_close_ind() until that confirmation arrives
    instead of firing it synchronously from avdt_ad_close_req().
    
    Also move the tcid-to-channel-type helper (renamed avdt_ad_tcid_to_type
    -> tc_tcid_to_type) from avdt_ad.cc into avdt_int.h so it can be used
    for logging in the new avdt_l2c.cc callback.
    
    Bug: 399308340
    Bug: 427109823
    Bug: 315241296
    Bug: 329809288
    
    CRs-Fixed: 4590108
    Change-Id: I9a5bb253f160148c429efe33a200ceb83fb088e3

--- system/stack/avdt/avdt_l2c.cc
+++ system/stack/avdt/avdt_l2c.cc
@@ -48,6 +48,7 @@ void avdt_l2c_connect_ind_cback(const RawAddress& bd_addr, uint16_t lcid, uint16
 void avdt_l2c_connect_cfm_cback(uint16_t lcid, tL2CAP_CONN result);
 void avdt_l2c_config_cfm_cback(uint16_t lcid, uint16_t result, tL2CAP_CFG_INFO* p_cfg);
 void avdt_l2c_config_ind_cback(uint16_t lcid, tL2CAP_CFG_INFO* p_cfg);
+void avdt_l2c_disconnect_cfm_cback(uint16_t lcid, uint16_t result);
 void avdt_l2c_disconnect_ind_cback(uint16_t lcid, bool ack_needed);
 void avdt_l2c_congestion_ind_cback(uint16_t lcid, bool is_congested);
 void avdt_l2c_data_ind_cback(uint16_t lcid, BT_HDR* p_buf);
@@ -59,7 +60,7 @@ const tL2CAP_APPL_INFO avdt_l2c_appl = {avdt_l2c_connect_ind_cback,
                                         avdt_l2c_config_ind_cback,
                                         avdt_l2c_config_cfm_cback,
                                         avdt_l2c_disconnect_ind_cback,
-                                        NULL,
+                                        avdt_l2c_disconnect_cfm_cback,
                                         avdt_l2c_data_ind_cback,
                                         avdt_l2c_congestion_ind_cback,
                                         NULL,
@@ -303,6 +304,30 @@ void avdt_l2c_config_ind_cback(uint16_t lcid, tL2CAP_CFG_INFO* p_cfg) {
     p_tbl->peer_mtu = L2CAP_DEFAULT_MTU;
   }
   log::verbose("lcid: 0x{:04x}, peer_mtu: {}", lcid, p_tbl->peer_mtu);
+}
+
+/*******************************************************************************
+ *
+ * Function         avdt_l2c_disconnect_cfm_cback
+ *
+ * Description      This is the L2CAP disconnect confirmation callback function.
+ *
+ *
+ * Returns          void
+ *
+ ******************************************************************************/
+void avdt_l2c_disconnect_cfm_cback(uint16_t lcid, uint16_t result) {
+  AvdtpTransportChannel* p_tbl;
+
+  /* look up info for this channel */
+  p_tbl = avdt_ad_tc_tbl_by_lcid(lcid);
+  if (p_tbl == NULL) {
+    log::warn("Adaptation layer transport channel table is NULL");
+    return;
+  }
+  log::verbose("type: {} lcid: 0x{:04x} result: 0x{:04x} ",
+               tc_type_text(tc_tcid_to_type(p_tbl->tcid)), lcid, result);
+  avdt_ad_tc_close_ind(p_tbl);
 }
 
 /*******************************************************************************
