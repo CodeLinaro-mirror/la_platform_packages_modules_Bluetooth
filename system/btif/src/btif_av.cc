1aa97a18356764a1d0324729e98d79b07e10fdae
Pranshu Bansal <pranbans@qti.qualcomm.com>
2026-08-04 12:11:57 +0530

    Add sysprops to configure cover art and browsing for AVRCP Controller.
    This also disables the feature. These are upstream changes taken from
    Android X.
    
    CRs-Fixed: 4623446
    
    Change-Id: I713cf42a09194e475575f9361d1fbd0dd4b1573a

--- system/btif/src/btif_av.cc
+++ system/btif/src/btif_av.cc
@@ -4607,8 +4607,14 @@ bt_status_t btif_av_sink_execute_service(bool enable) {
     // auto-suspend AV streaming on AG events (SCO or Call). The suspend shall
     // be initiated by the app/audioflinger layers.
     tBTA_AV_FEAT features = BTA_AV_FEAT_NO_SCO_SSPD | BTA_AV_FEAT_RCCT | BTA_AV_FEAT_METADATA |
-                            BTA_AV_FEAT_VENDOR | BTA_AV_FEAT_ADV_CTRL | BTA_AV_FEAT_RCTG |
-                            BTA_AV_FEAT_BROWSE | BTA_AV_FEAT_COVER_ARTWORK;
+                            BTA_AV_FEAT_VENDOR | BTA_AV_FEAT_ADV_CTRL | BTA_AV_FEAT_RCTG;
+
+    if (avrcp_controller_cover_art_enabled()) {
+      features |= BTA_AV_FEAT_COVER_ARTWORK;
+    }
+    if (avrcp_controller_browsing_enabled()) {
+      features |= BTA_AV_FEAT_BROWSE;
+    }
 
     if (delay_reporting_enabled()) {
       features |= BTA_AV_FEAT_DELAY_RPT;
